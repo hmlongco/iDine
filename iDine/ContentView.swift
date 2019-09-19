d1cc47954a11ac94079f63f6192f178b5e94379d
Michael Long <mlong@clientresourcesinc.com>
2019-09-19 12:55:31 -0500

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Michael Long on 9/19/19.
+//  Copyright © 2019 Michael Long. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
